0092c7d83506d535a6992de5fe644a9b9d178231
Jason Zeng <zengkaiting01@gmail.com>
2023-03-26 17:52:31 +1100

    Added color selector. Formatted Layout

--- grid.js
+++ grid.js
@@ -2,6 +2,10 @@
 const gridContainer = document.querySelector('.gridContainer');
 //If mouse clicked
 let drawEnable = false;
+//Default Width
+let selectedWidth = 64;
+//Default Color
+let selectedColor = "black";
 
 //Function to generate grids
 function generateGrid(width){
@@ -18,29 +22,50 @@ function generateGrid(width){
             gridDiv.setAttribute('id', k.toString() + i.toString())
             thisRow.appendChild(gridDiv);
         }
-    }
+    };
+    addListener();
+}
+
+function clearGrid(){
+    const rowDivs = [...document.querySelectorAll('.rowDiv')];
+    rowDivs.forEach(rowDiv => {
+        rowDiv.remove();
+    });
+    generateGrid(selectedWidth);
+    addListener;
+}
+
+function addListener(){
+    //Create change color event
+    //Get all grids
+    const grids = [...document.querySelectorAll('.gridDiv')];
+    //Add event listner to all grids
+    grids.forEach(gridDiv => {
+        gridDiv.addEventListener('mousedown',(e) =>{
+            e.target.style.background = selectedColor;
+            drawEnable = true;
+            console.log("Enable Draw");
+        });
+        gridDiv.addEventListener('mouseup',() =>{
+            drawEnable = false;
+            console.log("Disable Draw");
+        });
+        gridDiv.addEventListener('mouseover',(e) =>{
+            if(drawEnable){
+                e.target.style.background = selectedColor;
+            }
+        });
+    });
+}
+
+function changeSize(){
+    selectedWidth = document.getElementById("sizeSelector").value;
+    clearGrid(selectedWidth);
+}
+
+function changeColor(){
+    selectedColor = document.getElementById("colorPicker").value;
 }
 
 //Create 64 * 64 grids initially
-generateGrid(128);
-
-//Create change color event
-//Get all grids
-const grids = [...document.querySelectorAll('.gridDiv')];
-//Add event listner to all grids
-grids.forEach(gridDiv => {
-    gridDiv.addEventListener('mousedown',(e) =>{
-        e.target.style.background = "black";
-        drawEnable = true;
-        console.log("Enable Draw");
-    });
-    gridDiv.addEventListener('mouseup',() =>{
-        drawEnable = false;
-        console.log("Disable Draw");
-    });
-    gridDiv.addEventListener('mouseover',(e) =>{
-        if(drawEnable){
-            e.target.style.background = "black";
-        }
-    });
-});
+generateGrid(selectedWidth);
