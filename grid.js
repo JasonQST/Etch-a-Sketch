a61e93d3c6d2906e928493ea8a00e78cfb4242a5
Jason Zeng <zengkaiting01@gmail.com>
2023-03-26 15:24:04 +1100

    Draw only mouse clicked

--- grid.js
+++ grid.js
@@ -1,17 +1,46 @@
+//Get Container Object
 const gridContainer = document.querySelector('.gridContainer');
-//gridDiv.setAttribute('style','background-color:blue');
-for(k=0;k<16;k++){
-    let rowDiv = document.createElement('Div');
-    rowDiv.classList.add('rowDiv');
-    let thisRowID = "row" + k.toString();
-    rowDiv.setAttribute('id', thisRowID);
-    gridContainer.appendChild(rowDiv);
-    let thisRow = document.querySelector('#' + CSS.escape(thisRowID));
-    for(i=0;i<16;i++){
-        console.log(i);
-        const gridDiv = document.createElement('Div');
-        gridDiv.classList.add('gridDiv');
-        gridDiv.setAttribute('id', k.toString() + i.toString())
-        thisRow.appendChild(gridDiv);
+//If mouse clicked
+let drawEnable = false;
+
+//Function to generate grids
+function generateGrid(width){
+    for(k=0;k<width;k++){
+        let rowDiv = document.createElement('Div');
+        rowDiv.classList.add('rowDiv');
+        let thisRowID = "row" + k.toString();
+        rowDiv.setAttribute('id', thisRowID);
+        gridContainer.appendChild(rowDiv);
+        let thisRow = document.querySelector('#' + CSS.escape(thisRowID));
+        for(i=0;i<width;i++){
+            const gridDiv = document.createElement('Div');
+            gridDiv.classList.add('gridDiv');
+            gridDiv.setAttribute('id', k.toString() + i.toString())
+            thisRow.appendChild(gridDiv);
+        }
     }
 }
+
+//Create 64 * 64 grids initially
+generateGrid(128);
+
+//Create change color event
+//Get all grids
+const grids = [...document.querySelectorAll('.gridDiv')];
+//Add event listner to all grids
+grids.forEach(gridDiv => {
+    gridDiv.addEventListener('mousedown',(e) =>{
+        e.target.style.background = "black";
+        drawEnable = true;
+        console.log("Enable Draw");
+    });
+    gridDiv.addEventListener('mouseup',() =>{
+        drawEnable = false;
+        console.log("Disable Draw");
+    });
+    gridDiv.addEventListener('mouseover',(e) =>{
+        if(drawEnable){
+            e.target.style.background = "black";
+        }
+    });
+});
