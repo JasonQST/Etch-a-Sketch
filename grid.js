05981ca5047e5c203537c882c5fb78d3bb8006b8
Jason Zeng <zengkaiting01@gmail.com>
2023-03-26 13:30:32 +1100

    Create 16 * 16 Grids

--- grid.js
+++ grid.js
@@ -1,13 +1,17 @@
 const gridContainer = document.querySelector('.gridContainer');
 //gridDiv.setAttribute('style','background-color:blue');
 for(k=0;k<16;k++){
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('')
+    let rowDiv = document.createElement('Div');
+    rowDiv.classList.add('rowDiv');
+    let thisRowID = "row" + k.toString();
+    rowDiv.setAttribute('id', thisRowID);
+    gridContainer.appendChild(rowDiv);
+    let thisRow = document.querySelector('#' + CSS.escape(thisRowID));
     for(i=0;i<16;i++){
         console.log(i);
-        const gridDiv = document.createElement('div');
-        gridDiv.classList.add('gridDiv');
-        gridDiv.setAttribute('id', k.toString() + i.toString())
-        gridContainer.appendChild(gridDiv);
+        const gridSpan = document.createElement('Span');
+        gridSpan.classList.add('gridSpan');
+        gridSpan.setAttribute('id', k.toString() + i.toString())
+        thisRow.appendChild(gridSpan);
     }
 }
